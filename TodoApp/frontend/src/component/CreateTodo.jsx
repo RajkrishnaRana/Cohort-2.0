ddcd557c66c3b6bc3825b174f7db20308de83cc7
RajkrishnaRana <rajkrishnaranastudy@gmail.com>
2024-09-21 23:41:16 +0530

    todo app first version completed

--- TodoApp/frontend/src/component/CreateTodo.jsx
+++ TodoApp/frontend/src/component/CreateTodo.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function CreateTodo() {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
   return (
     <div>
-      <input type="text" placeholder="Title" />
+      <input
+        type="text"
+        placeholder="Title"
+        onChange={(e) => setTitle(e.target.value)}
+      />
       <br />
-      <input type="text" placeholder="description" />
+      <input
+        type="text"
+        placeholder="description"
+        onChange={(e) => setDesc(e.target.value)}
+      />
       <br />
-      <button style={{ marginTop: "5px" }}>Create</button>
+      <button
+        style={{ marginTop: "5px" }}
+        onClick={() => {
+          console.log({ title, desc });
+          fetch("http://localhost:3000/todo", {
+            method: "POST",
+            body: JSON.stringify({
+              title: title,
+              description: desc,
+            }),
+            headers: { "Content-type": "application/json" },
+          }).then(async (res) => {
+            const json = await res.json();
+            alert("Todo created");
+          });
+        }}
+      >
+        Create
+      </button>
     </div>
   );
 }
