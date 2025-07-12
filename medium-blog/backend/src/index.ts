f41248a23f5f6a9ac0c0fd215beb2d2f25c63c6c
Rajkrishna <rajkrishnaranastudy@gmail.com>
2025-07-12 13:35:00 +0530

    Successfully done prisma migrate, database connected successfully

--- medium-blog/backend/src/index.ts
+++ medium-blog/backend/src/index.ts
@@ -1,9 +1,54 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "@prisma/client/edge";
 
-const app = new Hono()
+const app = new Hono<{
+    Bindings: {
+        DATABASE_URL: string;
+    };
+}>();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/users/signup", async (c) => {
+    const body = await c.req.json();
 
-export default app
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    try {
+        await prisma.user.create({
+            data: {
+                userName: body.username,
+                password: body.password,
+                name: body.name,
+            },
+        });
+
+        return c.text("Success");
+    } catch (error) {
+        c.status(403);
+        return c.text("Error");
+    }
+});
+
+app.post("/api/v1/users/signin", (c) => {
+    return c.text("Hello Hono!");
+});
+
+app.post("/api/v1/blog", (c) => {
+    return c.text("Hello Hono!");
+});
+
+app.put("/api/v1/blog", (c) => {
+    return c.text("Hello Hono!");
+});
+
+app.get("/api/v1/blog", (c) => {
+    return c.text("Hello Hono! 2");
+});
+
+app.get("/api/v1/blog/bulk", (c) => {
+    return c.text("Hello Hono!");
+});
+
+export default app;
