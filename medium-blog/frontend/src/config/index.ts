153780bd0fa344470528eca8d01c5673fc0e1b48
Rajkrishna <rajkrishnaranastudy@gmail.com>
2025-10-06 09:04:46 +0530

    Update backend with production url

--- medium-blog/frontend/src/config/index.ts
+++ medium-blog/frontend/src/config/index.ts
@@ -1,4 +1,4 @@
 const LOCAL_BASE_URL = "http://localhost:8787";
-const PRODUCTION_BASE_URL = "";
+const PRODUCTION_BASE_URL = "https://backend.rajkrishnaranastudy.workers.dev";
 
-export const BASE_URL = LOCAL_BASE_URL;
+export const BASE_URL = PRODUCTION_BASE_URL;
